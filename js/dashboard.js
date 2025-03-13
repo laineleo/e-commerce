c2ebab61052f1de7ce5918ad32c73bea1ab083e3
Prince0643 <tolentinochristian89@gmail.com>
2025-03-13 13:11:01 +0800

    Recent Changes

--- js/dashboard.js
+++ js/dashboard.js
@@ -6,8 +6,26 @@ document.addEventListener("DOMContentLoaded", function () {
             event.preventDefault();
             const image = this.querySelector(".image-click");
             const imageLink = image.getAttribute("src");
+            const productName = this.querySelector("h5").innerText;
+            const rating = this.querySelector("i");
+            const ratingText = this.querySelector(".rating-text").innerText;
+            const description = this.querySelector(".described").innerText;
+            const productPrice = this.querySelector(".product-price").innerText;
+            const cleanedDescription = description.trim().replace(/\s+/g, ' ').replace(/\n/g, ' ');
 
-            sessionStorage.setItem("selectedImage", imageLink);
+            console.log("Cleaned Description before storing:", cleanedDescription);
+
+            const productDetails = {
+                image: imageLink,
+                name: productName,
+                rate: rating,
+                rateText: ratingText,
+                description: cleanedDescription,
+                price: productPrice
+            };
+            sessionStorage.setItem("selectedProduct", JSON.stringify(productDetails));
+
+            console.log(sessionStorage);
 
             window.location.href = "product-view.html";
         });
